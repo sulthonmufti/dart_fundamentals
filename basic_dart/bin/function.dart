1fe73c9d73e80f5bcea670f6f99a8e814af38fe2
sulthonmufti <msulthonmufti14@gmail.com>
2025-12-09 19:53:15 +0700

    add some new parameter function

--- basic_dart/bin/function.dart
+++ basic_dart/bin/function.dart
@@ -36,6 +36,40 @@ void defaultParamter({
   print('Hello $defaultDepan $defaultTengah $defaultBelakang');
 }
 
+//fuction dengan REQUIRED PARAMETER
+void reqiuredParamter({
+  required String reqiuredDepan,
+  String?
+  reqiuredTengah, //karena kita pake named paramater {} jadi paramater ini kan nullable jadi harus pake '?', kalau required sama default value gausah karena udah pasti ada isinya.
+  String reqiuredBelakang = 'Default',
+}) {
+  print('Hello $reqiuredDepan $reqiuredTengah $reqiuredBelakang');
+}
+
+//function return value
+//menghitung total data dalam list.
+int sum(List<int> numbers) {
+  var total = 0;
+  for (var value in numbers) {
+    total += value;
+  }
+  return total;
+}
+
+//contoh lain function return value
+String namaKU(String name) {
+  return 'Hello $name';
+}
+
+//FUNCTION SHORT EXPRESSION
+
+//biasanya kan kita gini ya:
+// int jumlah(int first, int second){
+//    return first = second;
+//}
+//bisa kita singkat menjadi seperti ini:
+int jumlah(int first, int second) => first + second;
+
 //MAIN FUNCTION
 void main() {
   /**
@@ -67,7 +101,7 @@ void main() {
   /**
    * OPTIONAL PARAMETER
    * secara default, parameter wajib dikirim saat kita panggil function.
-   * tapi jika kita mau buat perameter yang optional, artinya tidak wajib dikirim, kita wajib wrap salam kurum []
+   * tapi jika kita mau buat perameter yang optional, artinya tidak wajib dikirim, kita wajib wrap salam kurung []
    * dan parameter optional haruslah nullable.
    */
 
@@ -115,4 +149,59 @@ void main() {
   defaultParamter(defaultDepan: 'namaDepan', defaultTengah: 'namaTengah');
   defaultParamter(defaultTengah: 'namaTengah', defaultBelakang: 'namaBeakang');
   defaultParamter(defaultBelakang: 'namaBelakang');
+
+  print('==========');
+
+  /**
+   * REQUIRED PARAMETER
+   * intinya untuk memaksa sebuah paramter menjadi mandatory, jadi kita WAJIB menambahkan parameter kalo mau panggil function tersebut.
+   * caranya tambahkan kata kuncu 'required' di awal paramter.
+   */
+  reqiuredParamter(
+    reqiuredDepan: 'namaDepan',
+  ); //otomatis suruh ngisi value parameter reqiuredDepan, kalau engga nanti error
+  reqiuredParamter(reqiuredDepan: 'namaDepan', reqiuredTengah: 'namaTengah');
+  reqiuredParamter(
+    reqiuredDepan: 'namaDepan',
+    reqiuredBelakang: 'namaBelakang',
+  );
+
+  print('==========');
+
+  /**
+   * FUNCTION RETURN VALUE
+   * secara default kan saat kita buat function hasil value nya itu default adalah null, tapi kita juga bisa membuat mengambalikan valuenya tidak hanya null
+   * supaya function bisa menghasilkan value, kita harus mengubah kata kunci void dengan tipe daya yang dihasilkan, misal kita mau mengambalikan hasil dari function tersebut adalah string maka ganti void nya menjadi string.
+   * dan dalam block function nya untuk menghasilkan nilai tersebut, kita harus menggunakan kata kuncu return, lalu diikut-
+   * dengan data yang sesuai dengan tipe data yang sudah kita diklarasikan di function.
+   * kita hanya bisa menghasilkan 1 data di sebuah function, tidak bisa lebih dari satu.
+   */
+  print(sum([10, 10, 10, 10, 10]));
+  print(sum([5, 5, 5, 5, 5]));
+
+  var data = namaKU('Mufti');
+  print(data);
+
+  print('==========');
+
+  /**
+   * FUNCTION SHORT EXPRESSION
+   * dart mendukung function short expression
+   * saat kita  buat sebuah function yang hanya 1 baris, kita bisa menyingkatnya secara sederhana.
+   * untuk membuat fuction short expression, kita tidak butuh kurung{} dan jiga tidak butuh kata kunci return.
+   */
+  print(jumlah(10, 10));
+  print(jumlah(5, 9));
+
+  ///INNER FUNCTION
+  ///di dart kita bisa buat inner function di dalam outer function.
+  ///tapi perlu diperhatiinn, inner function yang dibuat di dalam outer function hanya bisa diakses dari outer dunction saja. tidak bisa diakses dari luar outer function.
+  ///detailnya ada di materi Scope.
+  void katakanHello() {
+    print('Hello Innter Function');
+  }
+
+  //void katakanHello ini bisa kita panggil didalam void main, tapi kalau kita punya function di luar void main tidak bisa memanggil function katakHello();
+  katakanHello();
+  katakanHello();
 }
